b1c4861b8cfcddd489a92188d007fa6be3a3fff3
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-07 08:49:34 -0700

    Add flash and scale
    Add flash and scale to laser and ship

--- player_ship/ship.gd
+++ player_ship/ship.gd
@@ -3,6 +3,7 @@ extends Node2D
 @onready var left_muzzle : Marker2D = $LeftMuzzle
 @onready var spawner_component : SpawnerComponent = $SpawnerComponent
 @onready var fire_rate_timer : Timer = $FireRateTimer
+@onready var scale_component : ScaleComponent = $ScaleComponent
 
 
 func _ready():
@@ -10,5 +11,6 @@ func _ready():
 	
 	
 func fire_lasers():
+	scale_component.tween_scale()
 	spawner_component.spawn(left_muzzle.global_position)
 	spawner_component.spawn(right_muzzle.global_position)
