b6fcc3fa760e8a6eb161e1b8a15939c48113d3b2
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-06 17:53:52 -0700

    Fix move_stats

--- components/move_input_component.gd
+++ components/move_input_component.gd
@@ -1,9 +1,9 @@
 class_name MoveInputComponent # Making it a class
 extends Node
 
-@export var move_stats: MoveStats
+#@export var move_stats : MoveStats
 @export var move_component: MoveComponent # Calling out the Move_Component Component
-
+@export var move_stats: MoveStats
 
 func _input(event: InputEvent): # Function to get the INPUT from an input event
 	var input_axis = Input.get_axis("ui_left","ui_right") # putting the left or right input into a variable
--- components/spawner_component.gd
+++ components/spawner_component.gd
@@ -1,4 +1,4 @@
-# Give the component a class name so it can be instanced as a custom node
+mm# Give the component a class name so it can be instanced as a custom node
 class_name SpawnerComponent
 extends Node2D
 
