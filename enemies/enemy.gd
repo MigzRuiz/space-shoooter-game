974f1f1e2c9625ea3e4a6e10694c0220b5daa59c
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-08 21:06:10 -0700

    Adding the hit and hurtboxes

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -1,12 +1,16 @@
 extends Node2D
 
-@onready var visible_on_screen_notifier_2d = $VisibleOnScreenNotifier2D
-@onready var move_component = $MoveComponent
-@onready var shake_component = $ShakeComponent
-@onready var stats_component = $StatsComponent
-@onready var scale_component = $ScaleComponent
-@onready var flash_component = $FlashComponent
+@onready var visible_on_screen_notifier_2d : VisibleOnScreenNotifier2D = $VisibleOnScreenNotifier2D
+@onready var move_component : MoveComponent= $MoveComponent
+@onready var shake_component : ShakeComponent = $ShakeComponent
+@onready var stats_component : StatsComponent = $StatsComponent
+@onready var scale_component : ScaleComponent = $ScaleComponent
+@onready var flash_component : FlashComponent = $FlashComponent
+@onready var hitbox_component : HitboxComponent = $HitboxComponent
+@onready var hurtbox_component : HurtboxComponent = $HurtboxComponent
 
 func _ready():
 	visible_on_screen_notifier_2d.screen_exited.connect(queue_free)
-
+	hurtbox_component.hurt.connect(func(hitbox : HitboxComponent):
+		queue_free()
+	)
