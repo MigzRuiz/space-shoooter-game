bdf3ee4423ea3e46f7d80c8843fca4e4b51fa905
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-19 20:59:21 -0700

    Added the Yellow Enemy

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -1,17 +1,20 @@
 extends Node2D
 
 @export var GreenEnemyScene: PackedScene
+@export var YellowEnemyScene : PackedScene
 
 var margin = 8
 var screen_width =  ProjectSettings.get_setting("display/window/size/viewport_width")
 
 @onready var spawner_component : SpawnerComponent = $SpawnerComponent
 @onready var green_enemy_spawn_timer : Timer = $GreenEnemySpawnTimer
+@onready var yellow_enemy_spawn_timer = $YellowEnemySpawnTimer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene,green_enemy_spawn_timer))# I need to do a bind on this beceause the timeout doesn't accept arguments
+	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene,yellow_enemy_spawn_timer))
 
 func handle_spawn(enemy_scene:PackedScene, timer):
 	spawner_component.scene = enemy_scene
