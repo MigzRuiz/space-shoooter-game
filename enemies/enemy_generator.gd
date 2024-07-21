05d6b1d7f534b327011ddb50a6518206b0656aa3
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-21 16:53:54 -0700

    Adding the Pink Enemy Projectiles

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -2,6 +2,7 @@ extends Node2D
 
 @export var GreenEnemyScene: PackedScene
 @export var YellowEnemyScene : PackedScene
+@export var PinkEnemyScene : PackedScene
 
 var margin = 8
 var screen_width =  ProjectSettings.get_setting("display/window/size/viewport_width")
@@ -9,12 +10,14 @@ var screen_width =  ProjectSettings.get_setting("display/window/size/viewport_wi
 @onready var spawner_component : SpawnerComponent = $SpawnerComponent
 @onready var green_enemy_spawn_timer : Timer = $GreenEnemySpawnTimer
 @onready var yellow_enemy_spawn_timer = $YellowEnemySpawnTimer
+@onready var pink_enemy_spawn_timer = $PinkEnemySpawnTimer
 
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene,green_enemy_spawn_timer))# I need to do a bind on this beceause the timeout doesn't accept arguments
 	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene,yellow_enemy_spawn_timer))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene,pink_enemy_spawn_timer))
 
 func handle_spawn(enemy_scene:PackedScene, timer):
 	spawner_component.scene = enemy_scene
--- enemies/pink_enemy.gd
+++ enemies/pink_enemy.gd
@@ -1,4 +1,4 @@
-class_name  PinkEnemy
+class_name PinkEnemy
 extends Enemy
 
 
@@ -7,6 +7,8 @@ extends Enemy
 @onready var move_side_state : TimedStateComponent = %MoveSideState
 @onready var pause_state : TimedStateComponent = %PauseState
 @onready var move_side_component : MoveComponent = %MoveSideComponent
+@onready var fire_state: StateComponent = %FireState
+@onready var projectile_spawner_component: SpawnerComponent = %ProjectileSpawnerComponent
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -21,7 +23,14 @@ func _ready():
 	
 	# Moving the states to move them from one state to another.
 	move_down_state.state_finished.connect(move_side_state.enable)
-	move_side_state.state_finished.connect(pause_state.enable)
+	move_side_state.state_finished.connect(func():
+		fire_state.enable()
+		scale_component.tween_scale()
+		projectile_spawner_component.spawn(global_position)
+		fire_state.disable()
+		fire_state.state_finished.emit()
+	)
+	fire_state.state_finished.connect(pause_state.enable)
 	pause_state.state_finished.connect(move_down_state.enable)
 	
 	# This will start the looping of the states
