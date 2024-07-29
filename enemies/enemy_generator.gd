e8679703a4c16ca1d86ae4b03d7b4250d06073df
Miguel Anthony Ruiz <migz.p.ruiz@gmail.com>
2024-07-28 18:56:01 -0700

    Increase Difficulty Curve

--- enemies/enemy_generator.gd
+++ enemies/enemy_generator.gd
@@ -4,6 +4,8 @@ extends Node2D
 @export var YellowEnemyScene : PackedScene
 @export var PinkEnemyScene : PackedScene
 
+@export var game_stats : GameStats
+
 var margin = 8
 var screen_width =  ProjectSettings.get_setting("display/window/size/viewport_width")
 
@@ -16,10 +18,21 @@ var screen_width =  ProjectSettings.get_setting("display/window/size/viewport_wi
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	green_enemy_spawn_timer.timeout.connect(handle_spawn.bind(GreenEnemyScene,green_enemy_spawn_timer))# I need to do a bind on this beceause the timeout doesn't accept arguments
-	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene,yellow_enemy_spawn_timer))
-	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene,pink_enemy_spawn_timer))
+	yellow_enemy_spawn_timer.timeout.connect(handle_spawn.bind(YellowEnemyScene,yellow_enemy_spawn_timer, 5.0))
+	pink_enemy_spawn_timer.timeout.connect(handle_spawn.bind(PinkEnemyScene,pink_enemy_spawn_timer, 10.0))
+	
+	# enemy will show up after a certain score
+	game_stats.score_changed.connect(func(new_score:int):
+		if new_score > 10:
+			yellow_enemy_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+		if new_score > 50:
+			pink_enemy_spawn_timer.process_mode = Node.PROCESS_MODE_INHERIT
+	)
+	
+	
 
-func handle_spawn(enemy_scene:PackedScene, timer):
+func handle_spawn(enemy_scene:PackedScene, timer: Timer, time_offset: float = 1.0):
 	spawner_component.scene = enemy_scene
 	spawner_component.spawn(Vector2(randf_range(margin, screen_width-margin), -16)) #randomize where the enemy will go: X is for left or right, Y is -16 above the screen.
-	timer.start()
+	var spawn_rate = time_offset / (0.5 + (game_stats.score * 0.01))
+	timer.start(spawn_rate + randf_range(0.25, 0.5))
